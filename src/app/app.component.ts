bc612553dc4e5864407ab017b0092e6e97423857
kerrongordon <kgpsounds.com@gmail.com>
2018-09-26 16:21:39 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sms-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'sms';
+}
